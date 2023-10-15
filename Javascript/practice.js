c9fce5d6bbb69962a88ccd7425e24807f955e283
Md.Jahid ul-Islam <145700501+JH-Jahid28@users.noreply.github.com>
2023-10-15 23:48:33 +0600

    Add files via upload

---
+++ Javascript/practice.js
@@ -0,0 +1,38 @@
+var rahim = 500;
+var to_buy_apple = 10;
+var to_buy_orange = 8;
+var price_of_apple = 20;
+var price_of_orange = 15;
+
+var apple_cost = to_buy_apple * price_of_apple;
+var orange_cost = to_buy_orange * price_of_orange;
+
+var total_cost = apple_cost + orange_cost;
+
+var back = rahim - total_cost;
+
+console.log(back);
+
+
+
+var number_of_subjects = 4;
+var math = 86;
+var chemistry = 80;
+var physics = 70;
+var bangla = 65;
+
+var total_numbers = math + chemistry + physics + bangla;
+
+var total_avg = total_numbers / number_of_subjects;
+
+// console.log(total_avg);
+
+
+
+
+var fname = 'Md Jahidul';
+var lname = 'Islam';
+
+var fulname = fname +' '+ lname;
+
+// console.log(fulname);
